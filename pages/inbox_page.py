21067103925a09b9ddf20040a53beb95deeae85d
latfamt <38280626+latfamt@users.noreply.github.com>
2018-12-04 13:09:29 +0300

    Update inbox_page.py

--- pages/inbox_page.py
+++ pages/inbox_page.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 
 ADD_MAIL_BTN = By.CSS_SELECTOR, ".aic div[role=button]"
